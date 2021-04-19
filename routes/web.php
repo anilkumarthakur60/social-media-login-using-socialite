892a09e38dfd9dbc6abc35f116e42fc614563e1e
anil kumar thakur <anilkumarthakur60@gmail.com>
2021-04-19 14:33:23 +0545

    commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
+Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social.oauth');
+Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback')->name('social.callback');
